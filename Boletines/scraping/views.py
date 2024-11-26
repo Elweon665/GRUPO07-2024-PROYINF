52567cb6c4a8882d6d39c58a6f86804e5324ee17
ZoddaxX <116638209+ZoddaxX@users.noreply.github.com>
2024-11-25 21:55:43 -0300

    Final 1.5

--- Boletines/scraping/views.py
+++ Boletines/scraping/views.py
@@ -11,33 +11,55 @@ from .services.scraping import scrap_agricology_article
 from .services.scraping import database_update
 
 def show_scrap(request):
-    '''
-    interests = scrap_urls()
-    return render(request, 'scrap_view.html', {'interests': interests})
-    '''
-    # interests = scrap_urls()
-    # interests = scrap_apnews()
-    # interests = scrap_aedyr()
-    interests = scrap_agricology()
+    page = request.GET.get('page', 'default')  # Si no se envía "page", usa "default"
+
+    # Decidir qué función de scraping ejecutar
+    if page == "1pointfive":
+        interests = scrap_urls() 
+    elif page == "apnews":
+        interests = scrap_apnews()  
+    elif page == "aedyr":
+        interests = scrap_aedyr()  
+    elif page == "agricology":
+        interests = scrap_agricology()  
+    else:
+        interests = []  
 
     return render(request, 'scrap_view.html', {'interests': interests})
 
 
 def show_content(request):
-    #interest = scrap_urls()
-    #interest = scrap_apnews()
-    #interest = scrap_aedyr()
-    interest = scrap_agricology()
-    interests = [scrap_agricology_article(inter['link']) for inter in interest]
-    values = []
-    for i in range(0,len(interest)):
-        values.append({'title': interest[i]['title'],'content': interests[i]['content']})
+    # Leer el parámetro 'page' del GET
+    page = request.GET.get('page', '-')
+    print(page)
 
-    return render(request, 'news_view.html', {'interest': values})
+    interest = []
+
+    # Seleccionar la función de scraping según la página
+    if page == "1pointfive":
+        interest = scrap_urls()
+        interests = [scrap_news(inter['link']) for inter in interest] if interest else []
+    elif page == "aedyr":
+        interest = scrap_aedyr()
+        interests = [scrap_aedyr_article(inter['link']) for inter in interest] if interest else []
+    elif page == "apnews":
+        interest = scrap_apnews()
+        interests = [scrap_apnews_article(inter['link']) for inter in interest] if interest else []
+    elif page == "agricology":
+        interest = scrap_agricology()
+        interests = [scrap_agricology_article(inter['link']) for inter in interest] if interest else []
+
+    values = [
+        {'title': interest[i]['title'], 'content': interests[i]['content']}
+        for i in range(len(interest))
+    ]
+
+    return render(request, 'news_view.html', {'interest': values, 'selected_page': page})
+
 
 def update_db(request):
     if request.method == "POST":
-        database_update()  # Llama a la función que deseas ejecutar
+        database_update() 
         return JsonResponse({"success": True, "message": "Base de datos actualizada correctamente"})
     return JsonResponse({"success": False, "message": "Método no permitido"}, status=405)
 
